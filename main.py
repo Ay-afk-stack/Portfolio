2674b44a9bc87e728d3ad80eb489bd4ed470d09d
ay-afk-stack <pakhrin.ayush404>
2025-03-31 11:11:45 +0545

    Create columns, images and background

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(layout = "wide")
+
+col1,col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png",width = 300)
+
+with col2:
+    st.title("Ayush Pakhrin")
+    content = """Hi, I am Ayush Pakhrin! I'm an aspiring Python developer on a journey to master coding and problem-solving. With a passion for learning and a curiosity for how things work, I'm exploring Python's versatility in web development, data analysis, automation, and beyond. This portfolio showcases my progress, projects, and the challenges I've tackled along the way."""
+    st.info(content)
