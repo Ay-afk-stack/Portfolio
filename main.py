f6ab2ff24e8b86e7f51a0a032e71414ff1af3ec4
ay-afk-stack <pakhrin.ayush404>
2025-03-31 11:17:14 +0545

    add description on the new columns

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ with col2:
     st.title("Ayush Pakhrin")
     content = """Hi, I am Ayush Pakhrin! I'm an aspiring Python developer on a journey to master coding and problem-solving. With a passion for learning and a curiosity for how things work, I'm exploring Python's versatility in web development, data analysis, automation, and beyond. This portfolio showcases my progress, projects, and the challenges I've tackled along the way."""
     st.info(content)
+
+content2= """Below you can find some of the apps I have built in Python. Feel Free to contact me!"""
+
+st.write(content2)
